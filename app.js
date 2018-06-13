d51f4c16e2e1f06021f64a7c5c935a46c155edb8
randy venturina <randy.venturina.c@gmail.com>
2018-06-14 01:48:58 +0800

    added package.json

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 
-const App = () => (
-  <Button variant="contained" color="primary">
-    Hello World
-  </Button>
-);
+function App() {
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
